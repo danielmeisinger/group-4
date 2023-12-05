9fcb9a77c844731204dc9e19fd45acdbef114952
danielmeisinger <a78907ae-7d05-45f5-b13f-e3e97f0ae7ae@simplelogin.com>
2023-12-05 17:16:21 +0100

    Hinzufügen von Schulferien

--- 0_DataPreparation/dataset.R
+++ 0_DataPreparation/dataset.R
@@ -15,9 +15,10 @@ sales_data$Weekday <- weekdays(sales_data$Datum)
 weather_data <- read_csv("Data/wetter.csv")
 kiwo_days <- read_csv("Data/kiwo.csv")
 holidays <- read_csv("Data/feiertage.csv")
+schulferien <- read_csv("Data/Schulferien_2.csv")
 
 # create a tibble with the sales data, weather data and kiwo days
-combined_data <- left_join(weather_data, kiwo_days, join_by(Datum)) %>% left_join(holidays, join_by(Datum)) %>% left_join(sales_data, join_by(Datum))
+combined_data <- left_join(weather_data, kiwo_days, join_by(Datum)) %>% left_join(holidays, join_by(Datum))  %>% left_join(sales_data, join_by(Datum)) %>% left_join(schulferien, join_by(Datum))
 
 # drop columns in which Feiertage and Umsatz is NA
 combined_data <- combined_data %>% filter(!is.na(Feiertag) | !is.na(Umsatz))
