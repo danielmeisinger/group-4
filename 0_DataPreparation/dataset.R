feb6111874a9458609a3eaf7df51225f2f944ccc
danielmeisinger <a78907ae-7d05-45f5-b13f-e3e97f0ae7ae@simplelogin.com>
2023-12-15 23:26:34 +0100

    Hinzufügen von Sylvester und Tage vor Ostern als Datenset, Berechnung des Umsatzes für den Test Datensatz mit den ids für das Hochladen auf Kaggle; adj. R value = 0.86 für den Trainings Datensatz & MSE von 2800 für den Validation Datensatz

--- 0_DataPreparation/dataset.R
+++ 0_DataPreparation/dataset.R
@@ -6,6 +6,8 @@ library(styler)
 library(skimr)
 library(DataExplorer)
 library(lubridate)
+library(tidyverse)
+library(zoo)
 
 # Importieren aller .csv Dateien im Ordner Data
 verkaufs_daten <- read_csv("Data/umsatzdaten_gekuerzt.csv")
@@ -19,7 +21,16 @@ thw_kiel <- read_csv("Data/thw_kiel.csv")
 test_ids <- read_csv("Data/test_ids.csv")
 
 # Hinzufügen von Wochentagen abhängig von dem Datum im Datensatz wetter_daten
-wetter_daten$Weekday <- weekdays(wetter_daten$Datum)
+wetter_daten$wochentag <- weekdays(wetter_daten$Datum)
+
+# variable_to_impute <- "Temperatur"
+#
+# impute_missing_linear <- function(x) {
+#   zoo::na.approx(x)
+# }
+#
+# gesamte_daten <- gesamte_daten %>%
+#   mutate(across(all_of(column_to_impute), impute_missing_linear))
 
 # Hinzufügen von Werten für die Jahreszeit abhängig von dem Datum
 wetter_daten <- wetter_daten %>%
@@ -30,8 +41,8 @@ wetter_daten <- wetter_daten %>%
   between(month(Datum), 9, 11) ~ "Herbst"
   ))
 
-# wetter_daten <- wetter_daten %>%
-#   mutate(monat = month(Datum))
+wetter_daten <- wetter_daten %>%
+  mutate(monat = month(Datum))
 
 # Definition von Temperaturgrenzen, um die Temperatur in Kategorien einzuteilen
 temperatur_grenzen <- c(-Inf, -8, 4, 15, 21, 28, Inf)
@@ -72,6 +83,10 @@ wetter_daten$wetter_kategorie <- cut(wetter_daten$Wettercode,
                                   labels = wetter_bezeichnung,
                                   include.lowest = TRUE)
 
+
+# merge the test ids with the combined data
+verkaufs_daten <- full_join(verkaufs_daten, test_ids, by = c("Datum", "Warengruppe"))
+
 # Ändern des Spaltennamens Feiertag zu feiertag
 colnames(feiertage)[colnames(feiertage) == "Feiertag"] <- "feiertag"
 
@@ -86,7 +101,6 @@ colnames(schulferien)[colnames(schulferien) == "Ferien"] <- "ferien"
 schulferien <- schulferien %>%
   mutate(ferien = ifelse(ferien == "Keine Ferien", 0, 1)) %>%
   filter(ferien != 0)
-
 
 # Ändern des Spaltennamens von Spiel zu holstein_spiel
 colnames(holstein_kiel)[colnames(holstein_kiel) == "Spiel"] <- "holstein_spiel"
@@ -141,23 +155,37 @@ arbeitslosenquote$arbeitslosenquote_kategorie <- cut(arbeitslosenquote$arbeitslo
                                      labels = alzahl_bezeichnung,
                                      include.lowest = TRUE)
 
+sylvester <- data.frame(
+  Datum = c("2012-12-31", "2013-12-31", "2014-12-31", "2015-12-31", "2016-12-31", "2017-12-31", "2018-12-31", "2019-12-31"),
+  sylvester = c(1,1,1,1,1,1,1,1)
+)
+
+tage_vor_ostern <- data.frame(
+  Datum = c("2012-04-05", "2013-03-28", "2014-04-15", "2014-04-16","2014-04-17", "2015-03-30", "2015-04-01", "2015-04-02", "2016-03-22", "2016-03-23", "2016-03-24", "2017-04-11", "2017-04-12", "2017-04-13", "2018-03-27", "2018-03-28", "2018-03-29", "2019-04-16", "2019-04-17", "2019-04-18"),
+  tage_vor_ostern = c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+)
+
+sylvester$Datum <- as.Date(sylvester$Datum, format = "%Y-%m-%d")
+
+tage_vor_ostern$Datum <- as.Date(tage_vor_ostern$Datum, format = "%Y-%m-%d")
 
 # Erstellung eines tibbles mit allen Datensätzen aufgrundlage der gemeinsamen Spalte 'Datum'
-gesamte_daten <- full_join(wetter_daten, verkaufs_daten, join_by(Datum)) %>%
+gesamte_daten <- full_join(wetter_daten, verkaufs_daten, by = c("Datum"))
+
+gesamte_daten <- gesamte_daten %>%
   full_join(feiertage, join_by(Datum)) %>%
   full_join(kiwo_tage, join_by(Datum)) %>%
   full_join(holstein_kiel, join_by(Datum)) %>%
   full_join(thw_kiel, join_by(Datum)) %>%
   full_join(schulferien, join_by(Datum), relationship = "many-to-many") %>%
-  full_join(verkaufsoffener_sonntag, join_by(Datum))
+  full_join(verkaufsoffener_sonntag, join_by(Datum)) %>%
+  full_join(sylvester, join_by(Datum)) %>%
+  full_join(tage_vor_ostern, join_by(Datum))
 
 gesamte_daten <- gesamte_daten %>%
   mutate(jahr = year(Datum)) %>%
   full_join(arbeitslosenquote, join_by(jahr)) %>%
   full_join(einwohnerzahl, join_by(jahr))
-
-# merge the test ids with the combined data
-# gesamte_daten <- merge(gesamte_daten, test_ids, by = c("Datum", "Warengruppe"), all = TRUE)
 
 # Setzen aller NA Werte auf 0
 gesamte_daten$feiertag[is.na(gesamte_daten$feiertag)] <- 0
@@ -167,10 +195,19 @@ gesamte_daten$thw_spiel[is.na(gesamte_daten$thw_spiel)] <- 0
 gesamte_daten$ferien[is.na(gesamte_daten$ferien)] <- 0
 gesamte_daten$flohmarkt[is.na(gesamte_daten$flohmarkt)] <- 0
 gesamte_daten$Umsatz[is.na(gesamte_daten$Umsatz)] <- 0
+gesamte_daten$jahreszeit[is.na(gesamte_daten$jahreszeit)] <- 0
+gesamte_daten$Bewoelkung[is.na(gesamte_daten$Bewoelkung)] <- 0
+gesamte_daten$sylvester[is.na(gesamte_daten$sylvester)] <- 0
+gesamte_daten$tage_vor_ostern[is.na(gesamte_daten$tage_vor_ostern)] <- 0
+
+# column_to_clean <- "Temperatur"
+
+# Remove rows with missing values in the specified column
+# gesamte_daten <- gesamte_daten[complete.cases(gesamte_daten[, column_to_clean]), ]
 
 # Herausfiltern aller Werte ohne Umsatzdaten, bspw. wenn geschlossen wegen Feiertagen oder wegen fehlenden Daten im Jahr 2012 und 2019
-gefilterte_daten <- gesamte_daten %>%
-  filter(Umsatz == 0 & feiertag == 1 | Umsatz > 0 & feiertag == 0 | Umsatz > 0 & feiertag == 1)
+# gefilterte_daten <- gesamte_daten %>%
+#   filter(Umsatz == 0 & feiertag == 1 | Umsatz > 0 & feiertag == 0 | Umsatz > 0 & feiertag == 1)
 
 # Filtern nach einzigartigen Werten
-gefilterte_daten <- distinct(gefilterte_daten)
+gefilterte_daten <- distinct(gesamte_daten)
--- 2_BaselineModel/lm_baseline.R
+++ 2_BaselineModel/lm_baseline.R
@@ -7,11 +7,16 @@ umsatz_prediction_training_data <- gefilterte_daten %>%
   filter(Datum >= "2013-07-01" & Datum <= "2017-07-31")
 
 umsatz_prediction_validation_data <- gefilterte_daten %>%
-  filter(Datum >= "2017-08-01" & Datum <= "2018-07-13")
+  filter(Datum >= "2017-08-01" & Datum <= "2018-07-31")
+
+umsatz_prediction_test_data <- gefilterte_daten %>%
+  filter(Datum >= "2018-08-01" & Datum <= "2019-07-30")
 
 
 # Set up a linear model for the training dataset
-lm_umsatz_prediction_training_data <- lm(Umsatz ~ Weekday + Temperatur + Wettercode + as.factor(temperatur_kategorie) * as.factor(wind_kategorie) * as.factor(Warengruppe) * as.factor(KielerWoche)  * as.factor(feiertag) * as.factor(ferien) + as.factor(jahreszeit) + as.factor(holstein_spiel) * as.factor(thw_spiel) * as.factor(flohmarkt) * as.factor(temperatur_kategorie) * as.factor(jahreszeit) + as.factor(arbeitslosenquote_kategorie) + einwohnerzahl, data = umsatz_prediction_training_data)
+lm_umsatz_prediction_training_data <- lm(Umsatz ~ as.factor(wochentag) + Temperatur * Wettercode + Bewoelkung + as.factor(temperatur_kategorie) * as.factor(wind_kategorie) * as.factor(Warengruppe) * as.logical(KielerWoche)  * as.logical(feiertag) * as.logical(ferien) + as.factor(jahreszeit) * wochentag + as.logical(holstein_spiel) * as.logical(thw_spiel) * as.logical(flohmarkt) * as.factor(temperatur_kategorie) + as.factor(jahreszeit) + as.factor(arbeitslosenquote_kategorie) + as.factor(monat) + as.logical(sylvester) * as.factor(Warengruppe) + as.logical(tage_vor_ostern) * as.factor(Warengruppe), data = umsatz_prediction_training_data)
+
+# lm_umsatz_prediction_training_data <- lm(Umsatz ~ Datum + Datum * Temperatur + as.factor(Warengruppe) + as.factor(wochentag) + as.logical(ferien) + Temperatur + as.logical(KielerWoche) + Bewoelkung + Windgeschwindigkeit +  as.factor(monat) + as.factor(monat) * Temperatur + Windgeschwindigkeit * Temperatur, data = umsatz_prediction_training_data)
 
 # regression diagnostics
 summary(lm_umsatz_prediction_training_data)
@@ -19,6 +24,24 @@ summary(lm_umsatz_prediction_training_data)
 # plot(lm_umsatz_prediction_training_data)
 
 # calculate the values for the predicted Umsatz using the validation dataset and compare with the real values using mean squared error
-umsatz_prediction_validation_data$Umsatz_prediction <- predict(lm_umsatz_prediction_training_data, umsatz_prediction_validation_data)
-mse <- mean((umsatz_prediction_validation_data$Umsatz - umsatz_prediction_validation_data$Umsatz_prediction)^2, na.rm = TRUE)
+umsatz_prediction_validation_data$umsatz_prediction <- predict(lm_umsatz_prediction_training_data, umsatz_prediction_validation_data)
+mse <- mean((umsatz_prediction_validation_data$Umsatz - umsatz_prediction_validation_data$umsatz_prediction)^2, na.rm = TRUE)
 print(mse)
+
+
+umsatz_prediction_validation_data <- umsatz_prediction_validation_data %>%
+  mutate(Difference = abs(Umsatz - umsatz_prediction))
+
+# Find the row with the biggest difference
+max_difference_row <- umsatz_prediction_validation_data[which.max(umsatz_prediction_validation_data$Difference), ]
+
+# Print the date with the biggest difference
+print(max_difference_row$Datum)
+
+umsatz_prediction_test_data$umsatz_prediction <- predict(lm_umsatz_prediction_training_data, umsatz_prediction_test_data)
+
+kaggle_submission <- umsatz_prediction_test_data %>%
+  select(id, umsatz_prediction) %>%
+  rename(Umsatz = umsatz_prediction)
+
+kaggle_submission <-  kaggle_submission[complete.cases(kaggle_submission$id), ]
